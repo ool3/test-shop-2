153487f8afe11ca0594ec4fa151d7cc7ae5351bc
Mr.Pink <56068154+ool3@users.noreply.github.com>
2020-08-17 15:24:29 +0300

    Update settings.py

--- ShopWay/settings.py
+++ ShopWay/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')njutz(*#%hdq5tf)j!_!i^^qtghe19ie!d4yn_(2!8nit04r5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
