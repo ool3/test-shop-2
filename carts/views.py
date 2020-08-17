d9a853ad9163708993c5888355f3960c6a364e1c
Mr.Pink <56068154+ool3@users.noreply.github.com>
2020-08-17 15:40:15 +0300

    Update views.py

--- carts/views.py
+++ carts/views.py
@@ -198,19 +198,19 @@ def update_price(request):
 			print(response.content)
 		else:
 			print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
-		time.sleep(1000)		
+		time.sleep(300)		
 		r = requests.get(f'https://cbr.ru/')
-		time.sleep(1000)
+		time.sleep(300)
 		html = BS(r.content, 'html.parser')
-		time.sleep(1000)
+		time.sleep(300)
 		base_page = html.select('.row.flex-nowrap.home-indicators_items')
-		time.sleep(1000)
+		time.sleep(300)
 		for page in base_page:
-			time.sleep(1000)
+			time.sleep(300)
 			page_block = page.select('.indicator_el.indicator_course')
 		counter = 0
 		for block in page_block:
-			time.sleep(1000)
+			time.sleep(300)
 			if counter == 1:
 				euro = [i.text[:-1] for i in block.select('.indicator_el_value.mono-num')]
 				number_euro = float(euro[-1].replace(',', '.'))
